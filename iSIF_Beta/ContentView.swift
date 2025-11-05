0e441c6d7ee71dd85b4c3f24c03882f67ae299a7
Reginald Simon <reggie@rnatechco.com>
2025-11-04 18:50:37 -0800

    Initial Commit

---
+++ iSIF_Beta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSIF_Beta
+//
+//  Created by Reginald Simon on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
